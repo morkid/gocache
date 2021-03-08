01d0c4c76cfdea190346d59cea19fc60ed938ca3
morkid <morkid@gmail.com>
2021-03-08 10:53:35 +0700

    update interface comment

--- gocache.go
+++ gocache.go
@@ -8,7 +8,7 @@ type AdapterInterface interface {
 	Get(key string) (string, error)
 	// IsValid check if cache is valid
 	IsValid(key string) bool
-	// ClearPrefix clear cache by key
+	// Clear clear cache by key
 	Clear(key string) error
 	// ClearPrefix clear cache by key prefix
 	ClearPrefix(keyPrefix string) error
